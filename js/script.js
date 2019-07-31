8c65f62c936de79443d729e2fc86ebf7a119a245
Reyan <reyanrahman@gmail.com>
2019-08-01 01:31:13 +0530

    adding about page

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ let xScrollPosition,yScrollPosition;
 
 let HeyText=document.getElementById("hey"),
  sqr1=document.getElementById("sqr1"),
+ cir1=document.getElementById("cir1"),
+ cross1=document.getElementById("cross1"),
  navEl=document.getElementById("navbar").children[0], 
  navElgallery=navEl.children[3],
  greetback=document.getElementById("greet"),
  greetText=greet.children[0],
+ skillsBack=document.getElementById("skills_description"),
+ skillsText=skillsBack.children[0],
  descriptionback=document.getElementById("description"),
- descriptionText=descriptionback.children[0];
+ descriptionText=descriptionback.children[0],
+ aboutDescriptionBack=document.getElementById("about_description"),
+ aboutDescriptionText=aboutDescriptionBack.children[0],
+ AboutText=document.getElementById("about_big_text");
 
  let k=500;
 
@@ -23,14 +30,27 @@ function scrollLoop(){
     setTranslate(0,yScrollPosition*0.1,descriptionText);
     setTranslate(yScrollPosition*-1,yScrollPosition*-0.1,descriptionback);
     
+    /************For About***********/
+    if(yScrollPosition<innerHeight){
+        setTranslate(yScrollPosition*-1+innerHeight,yScrollPosition*-0.1,skillsBack);
+        setTranslate(0,yScrollPosition*0.1+innerHeight,skillsText);
+        setTranslate(0,yScrollPosition*-1.5+innerHeight,AboutText);
+        setTranslate(0,yScrollPosition*-0.01, aboutDescriptionText );
+        setTranslate(yScrollPosition*1-innerHeight,yScrollPosition*-0.1,aboutDescriptionBack);
+    }
     setTranslate(0,yScrollPosition*-1.5,HeyText);
-    setTranslate(0,yScrollPosition*0.2,sqr1,yScrollPosition);
+    
 
-    if(yScrollPosition<(innerWidth-500))
+    setTranslate(0,yScrollPosition*0.2,sqr1,yScrollPosition);
+    setTranslate(0,yScrollPosition*0.2,cir1,yScrollPosition)
+    setTranslate(0,yScrollPosition*0.2,cross1,yScrollPosition)
+    
+
+    if(yScrollPosition<(innerWidth-480))
     setTranslate(yScrollPosition,0,navEl);
 
-    if(500-yScrollPosition>=6)
-    navElgallery.style.paddingBottom=`${500-yScrollPosition}px`
+    if(800-(yScrollPosition*1.12)>=6)
+    navElgallery.style.paddingBottom=`${800-(yScrollPosition*1.12)}px`
 
     requestAnimationFrame(scrollLoop);
 }
