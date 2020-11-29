98081b9435d436aec87c7ebb4ccb6050e3215ef1
Reyzartz <reyanrahman@gmail.com>
2020-11-29 13:55:21 +0530

    Updates

--- js/ProjectsData.js
+++ js/ProjectsData.js
@@ -1,5 +1,34 @@
 const ProjectsData = [
     {
+        name:"Landing-page",
+        description:"A landing page made in HTML, CSS, and vanilla Javascript, which gives the user a random background or they can add tags of the category of images they want, user can also get weather information and can also access multiple other websites from it",
+        imgSrc:"./assets/images/landing-page.png",
+        srcCode:"https://github.com/Reyzartz/Landing-page",
+        link:"https://reyzartz.github.io/Landing-page/"
+
+    },
+    {
+        name:"tetris-Gameboy-made-in-JavaScript",
+        description:"Tetris made in vanilla JavaScript using p5js as for graphics. it's made to look like the original Gameboy version(The First Tetris game) with giving the user the experience and feel for the original tetris game",
+        imgSrc:"./assets/images/tetris.png",
+        srcCode:"https://github.com/Reyzartz/tetris-Gameboy-made-in-JavaScript",
+        link:"https://tetrisgameboy.netlify.app/"
+    },
+    {
+        name:"visual-lab-Editor",
+        description:"Visual lab editor is a web editor for the browser to run and write JS, HTML, CSS code with a live preview option available use get one HTML, CSS file and multiple JS files to add to there project they can also store their project in the Local Storage so the user can start where they left off. This project was made using ReactJS",
+        imgSrc:"./assets/images/visual_lab_editor.png",
+        srcCode:"https://github.com/Reyzartz/React-and-firebase-instant-messaging-application",
+        link:"https://reyzartz.github.io/visual_lab_Editor/"
+    },
+    {
+        name: "React-and-firebase-instant-messaging-application",
+        description:"a simple react based multi-user message application in which user can log in,sign-up, and message each other with firebase use as the back-end.",
+        imgSrc:"./assets/images/react-firebase.png",
+        srcCode:"https://github.com/Reyzartz/React-and-firebase-instant-messaging-application",
+        link:"https://react-firebase-messageing.netlify.app/"
+    },
+    {
         name: "News-Snippets",
         description:"A font-end react js based website that uses News as a source for it Articles. One can Search, Choose, Country and sort the articles in the website. it also include other npm-packages like react-router and axios.",
         imgSrc:"./assets/images/NewsSnippets1.png",
